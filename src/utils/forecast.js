f4c9037113fa9e880998ba4106668cb51a8df164
pritesh_1110 <flash.speedster11@gmail.com>
2020-04-08 17:56:43 +0530

    Added humidity text in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback("Unable to find location", undefined)
         } else{
-            callback(undefined, body.daily.data[0].summary+ "It is currently " +body.currently.temperature+ " degrees out. There is a "+body.currently.precipProbability+ "% chance of rain")
+            callback(undefined, body.daily.data[0].summary+ "It is currently " +body.currently.temperature+ " degrees out. Currently the humidity is " +body.daily.data[0].humidity +". There is a "+body.currently.precipProbability+ "% chance of rain")
         }
     })
 }
